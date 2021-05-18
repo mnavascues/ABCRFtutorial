43b1f6f909be32668639900d6713bbd3aae8452c
Miguel Navascués <miguel.navascues@inrae.fr>
2021-05-18 22:06:09 +0200

    added functions to estimate likelihood and posterior from simulations

--- coin_toss.R
+++ coin_toss.R
@@ -3,3 +3,23 @@ flip_coin_likelihood <- function(p,h,t){
   likelihood <- choose(h+t,h) * p^h * (1-p)^t
   return (list(p=p,l=likelihood))
 }
+
+flip_coin_likelihood_approx <- function(p,h,t,num_of_sim){
+  likelihood <- array(dim=length(p))
+  for (i in seq_along(p)){
+    likelihood[i] <- sum(rbinom(n=num_of_sim,size=h+t,prob=p[i])==h)/num_of_sim  
+  }
+  return (list(p=p,l=likelihood))
+}
+
+flip_coin_posterior_approx <- function(h,t,a,b,num_of_sim){
+  p_prime <- rbeta(num_of_sim,a,b)
+  d_prime <- rbinom(num_of_sim,h+t,p_prime)
+  p_prime_accept <- p_prime[which(d_prime==h)]
+  d_prime_accept <- d_prime[which(d_prime==h)]
+  return(list(p_prime=p_prime,
+              d_prime=d_prime,
+              p_prime_accept=p_prime_accept,
+              d_prime_accept=d_prime_accept))
+}
+
