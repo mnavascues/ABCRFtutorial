bc1f15214986d5a5d0664955fdb6cf8a20bf4661
Miguel Navascués <miguel.navascues@inrae.fr>
2021-05-24 16:16:58 +0200

    correction of errors when loading reference table

--- src/coin_toss.R
+++ src/coin_toss.R
@@ -4,17 +4,17 @@ flip_coin_likelihood <- function(p,h,t){
   return (list(p=p,l=likelihood))
 }
 
-flip_coin_likelihood_approx <- function(p,h,t,num_of_sim){
+flip_coin_likelihood_approx <- function(p,h,t,num_of_sims){
   likelihood <- array(dim=length(p))
   for (i in seq_along(p)){
-    likelihood[i] <- sum(rbinom(n=num_of_sim,size=h+t,prob=p[i])==h)/num_of_sim  
+    likelihood[i] <- sum(rbinom(n=num_of_sims,size=h+t,prob=p[i])==h)/num_of_sims  
   }
   return (list(p=p,l=likelihood))
 }
 
-flip_coin_posterior_approx <- function(h,t,a,b,num_of_sim){
-  p_prime <- rbeta(num_of_sim,a,b)
-  d_prime <- rbinom(num_of_sim,h+t,p_prime)
+flip_coin_posterior_approx <- function(h,t,a,b,num_of_sims){
+  p_prime <- rbeta(num_of_sims,a,b)
+  d_prime <- rbinom(num_of_sims,h+t,p_prime)
   p_prime_accept <- p_prime[which(d_prime==h)]
   d_prime_accept <- d_prime[which(d_prime==h)]
   return(list(p_prime=p_prime,
